5559f5ff09c0a30aae46e4aebe5d7137077664f2
Steven Bowen <steven.bowen@vtex.com.br>
2020-10-22 13:49:21 -0400

    Add store conditional block

---
+++ react/StoreConditional.tsx
@@ -0,0 +1,75 @@
+// import React from 'react'
+import React, { Fragment } from 'react'
+import { defineMessages } from 'react-intl'
+
+import { useStoreGroup } from './StoreGroup'
+
+interface StoreCustomFieldProps {
+  id: string
+  type: string
+}
+
+const StoreConditional: StorefrontFunctionComponent<StoreCustomFieldProps> = (
+  props
+) => {
+  const group = useStoreGroup()
+  const { id, type } = props
+
+  if (!group || !id) {
+    return null
+  }
+
+  if (type === 'text') {
+    const text = group.customFields.text.find((field) => field.id === id)
+
+    if (text?.data) {
+      return <Fragment>{props.children}</Fragment>
+    }
+  }
+
+  if (type === 'textList') {
+    const text = group.customFields.textList.find((field) => field.id === id)
+
+    if (text?.data.length) {
+      return <Fragment>{props.children}</Fragment>
+    }
+  }
+
+  if (type === 'image') {
+    const image = group.customFields.image.find((field) => field.id === id)
+
+    if (image?.data) {
+      return <Fragment>{props.children}</Fragment>
+    }
+  }
+
+  if (type === 'imageList') {
+    const images = group.customFields.imageList.find((field) => field.id === id)
+
+    if (images?.data?.length) {
+      return <Fragment>{props.children}</Fragment>
+    }
+  }
+
+  return null
+}
+
+const messages = defineMessages({
+  title: {
+    defaultMessage: '',
+    id: 'admin/editor.StoreConditional.title',
+  },
+  description: {
+    defaultMessage: '',
+    id: 'admin/editor.StoreConditional.description',
+  },
+})
+
+StoreConditional.schema = {
+  title: messages.title.id,
+  description: messages.description.id,
+  type: 'object',
+  properties: {},
+}
+
+export default StoreConditional
